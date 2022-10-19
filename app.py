30de4d44d92207d5d567e94a8fd091ff33430205
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2022-10-19 16:05:11 -0400

    cool interpretation

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ from constants import API_KEY
 
 co = cohere.Client(API_KEY)
 
-from cohere.classify import Example
 
-examples =  [Example("Do you offer same day shipping?", "Shipping and handling policy"),
-Example("Can you ship to Italy?", "Shipping and handling policy"),
-Example("How long does shipping take?", "Shipping and handling policy"),
-Example("Can I buy online and pick up in store?", "Shipping and handling policy"),
-Example("What are your shipping options?", "Shipping and handling policy"),
-Example("My order arrived damaged, can I get a refund?", "Start return or exchange"),
-Example("You sent me the wrong item", "Start return or exchange"),
-Example("I want to exchange my item for another colour", "Start return or exchange"),
-Example("I ordered something and it wasn't what I expected. Can I return it?", "Start return or exchange"),
-Example("What's your return policy?", "Start return or exchange"),
-Example("Where's my package?", "Track order"),
-Example("When will my order arrive?", "Track order"),
-Example("What's my shipping number?", "Track order"),
-Example("Which carrier is my package with?", "Track order"),
-Example("Is my package delayed?", "Track order")]
+examples=[Example("The order came 5 days early", "positive"), 
+            Example("The item exceeded my expectations", "positive"), 
+            Example("I ordered more for my friends", "positive"), 
+            Example("I would buy this again", "positive"), 
+            Example("I would recommend this to others", "positive"), 
+            Example("The package was damaged", "negative"), 
+            Example("The order is 5 days late", "negative"), 
+            Example("The order was incorrect", "negative"), 
+            Example("I want to return my item", "negative"), 
+            Example("The item\'s material feels low quality", "negative"), 
+            Example("The product was okay", "neutral"), 
+            Example("I received five items in total", "neutral"), 
+            Example("I bought it from the website", "neutral"), 
+            Example("I used the product this morning", "neutral"), 
+            Example("The product arrived yesterday", "neutral")]
 
-inputs=[" Am I still able to return my order?",
-        "When can I expect my package?",
-        "Do you ship overseas?",
-        ]
+inputs=["This item was broken when it arrived", 
+"The product is amazing", 
+"The product was not too bad",
+"I love this!"]
+
 response = co.classify(
     model='medium',
     inputs=inputs,
