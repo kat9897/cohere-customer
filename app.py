c425c3451170e79928d0e9f56faae5e82906108e
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2022-10-19 15:57:55 -0400

    working intent-recognition

--- app.py
+++ app.py
@@ -4,29 +4,28 @@ from constants import API_KEY
 
 co = cohere.Client(API_KEY)
 
-examples=[Example("How do I find my insurance policy?", "Finding policy details"),
-Example("How do I download a copy of my insurance policy?", "Finding policy details"),
-Example("How do I find my policy effective date?", "Finding policy details"),
-Example("When does my insurance policy end?", "Finding policy details"),
-Example("Could you please tell me the date my policy becomes effective?", "Finding policy details"),
-Example("How do I sign up for electronic filing?", "Change account settings"),
-Example("How do I change my policy?", "Change account settings"),
-Example("How do I sign up for direct deposit?", "Change account settings"),
-Example("I want direct deposit. Can you help with that?", "Change account settings"),
-Example("Could you deposit money into my account rather than mailing me a physical cheque?", "Change account settings"),
-Example("How do I file an insurance claim?", "Filing a claim and viewing status"),
-Example("How do I file a reimbursement claim?", "Filing a claim and viewing status"),
-Example("How do I check my claim status?", "Filing a claim and viewing status"),
-Example("When will my claim be reimbursed?", "Filing a claim and viewing status"),
-Example("I filed my claim 2 weeks ago but I still haven’t received a deposit for it.", "Filing a claim and viewing status"),
-Example("I want to cancel my policy immediately! This is nonsense.", "Cancelling coverage"),
-Example("Could you please help my end my insurance coverage? Thank you.", "Cancelling coverage"),
-Example("Your service sucks. I’m switching providers. Cancel my coverage.", "Cancelling coverage"),
-Example("Hello there! How do I cancel my coverage?", "Cancelling coverage"),
-Example("How do I delete my account?", "Cancelling coverage")]
+from cohere.classify import Example
 
-inputs=["I want to change my password","Does my policy cover prescription medication?"]
+examples =  [Example("Do you offer same day shipping?", "Shipping and handling policy"),
+Example("Can you ship to Italy?", "Shipping and handling policy"),
+Example("How long does shipping take?", "Shipping and handling policy"),
+Example("Can I buy online and pick up in store?", "Shipping and handling policy"),
+Example("What are your shipping options?", "Shipping and handling policy"),
+Example("My order arrived damaged, can I get a refund?", "Start return or exchange"),
+Example("You sent me the wrong item", "Start return or exchange"),
+Example("I want to exchange my item for another colour", "Start return or exchange"),
+Example("I ordered something and it wasn't what I expected. Can I return it?", "Start return or exchange"),
+Example("What's your return policy?", "Start return or exchange"),
+Example("Where's my package?", "Track order"),
+Example("When will my order arrive?", "Track order"),
+Example("What's my shipping number?", "Track order"),
+Example("Which carrier is my package with?", "Track order"),
+Example("Is my package delayed?", "Track order")]
 
+inputs=[" Am I still able to return my order?",
+        "When can I expect my package?",
+        "Do you ship overseas?",
+        ]
 response = co.classify(
     model='medium',
     inputs=inputs,
