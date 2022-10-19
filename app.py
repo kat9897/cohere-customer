d7564c41da16947aa41a60f9753abe1d1ce7a47a
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2022-10-19 16:08:55 -0400

    generated three different response summaries with the same info. Pretty cool:

--- app.py
+++ app.py
@@ -5,31 +5,26 @@ from constants import API_KEY
 co = cohere.Client(API_KEY)
 
 
-examples=[Example("The order came 5 days early", "positive"), 
-            Example("The item exceeded my expectations", "positive"), 
-            Example("I ordered more for my friends", "positive"), 
-            Example("I would buy this again", "positive"), 
-            Example("I would recommend this to others", "positive"), 
-            Example("The package was damaged", "negative"), 
-            Example("The order is 5 days late", "negative"), 
-            Example("The order was incorrect", "negative"), 
-            Example("I want to return my item", "negative"), 
-            Example("The item\'s material feels low quality", "negative"), 
-            Example("The product was okay", "neutral"), 
-            Example("I received five items in total", "neutral"), 
-            Example("I bought it from the website", "neutral"), 
-            Example("I used the product this morning", "neutral"), 
-            Example("The product arrived yesterday", "neutral")]
+prompt = f"""Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn't the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to "the dusty section of the dictionary" to find its latest words.
 
-inputs=["This item was broken when it arrived", 
-"The product is amazing", 
-"The product was not too bad",
-"I love this!"]
+TLDR: Wordle has not gotten more difficult to solve.
+--
+Passage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.
 
-response = co.classify(
-    model='medium',
-    inputs=inputs,
-    examples=examples)
+TLDR: ArtificialIvan has raised $190 million in Series C funding.
+--
+Passage: The National Weather Service announced Tuesday that a freeze warning is in effect for the Bay Area, with freezing temperatures expected in these areas overnight. Temperatures could fall into the mid-20s to low 30s in some areas. In anticipation of the hard freeze, the weather service warns people to take action now.
 
-print(response.classifications)
+TLDR:"""
+
+response = co.generate( 
+    model='xlarge', 
+    prompt = prompt,
+    max_tokens=40, 
+    temperature=0.8,
+    stop_sequences=["--"])
+
+summary = response.generations[0].text
+
+print(summary)
 
